6b1675ae23e628c7fd6f8a05136a617c3811ada9
JulieCapucine <jujucapucine.jp@gmail.com>
2017-02-19 19:24:39 +0100

    post components changed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,57 @@
 <template>
   <section class="article">
-    <p> tutututu </p>
-    <form-article :user="user"></form-user>
-    <pre>{{user}}</pre>
-
-  </div>
-
-  
-    
-
-
+    <post-template :post="post"></post-template>
+    <pre>{{post}}</pre>
+    <create-post :post="post"></create-post>
   </section>
 </template>
 
 <script>
-let formArticle = {
+let postTemplate = {
   props:{
-    user:Object
+    post:Object
   },
-  template: {
+  template:
+    `
+    <div class="post row"> 
+      <div class="col-sm-9">
+          <img src="../../www/assets/images/Avatar1.svg" alt="default avatar" class="col-sm-3">
+          <h1 class="pseudo">{{ post.user }}</h1>
+          <small> {{ post.date + post.hour }}</small>
+          <p> {{ post.content }} </p>
+      <div class="inter">
+        <img src="/assets/images/Like.svg">
+        <span> {{ post.likes }} </span>
+        <img src="/assets/images/Comments.svg"> 
+        <span> {{ post.comments }}</span>
+      </div>
+    </div>
+  </div>
+    `
+}
 
-  }
+let createPost = {
+  props:{
+    post:Object
+  },
+  template:
+    `
+    <form class="form" @submit.prevent="save">
+    <img src="../../www/assets/images/Avatar1.svg" alt="Default avatar">
+    <input type="text" placeholder="Publier quelque chose ..">
+    <img src="../../www/assets/images/picture.svg" alt="Default avatar">
+    <button class="button" type="submit">Publier</button> 
+    <p><slot name="footer"></slot></p>
+    </form>
+    `
 }
   export default {
-  components: {formArticle },
+  components: { postTemplate, createPost },
   data () {
       return {
-        user:{
-          pseudo: 'Lucky',
+        post:{
+          user: 'Lucky',
+          avatar : 'AVATAR',
           date: '20 fev',
           hour: '12h04',
           content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tortor eu ipsum laoreet faucibus. Etiam mattis eros id leo maximus blandit. Proin id massa in risus gravida suscipit non eu arcu. Aenean auctor lacus risus, porttitor sodales odio vehicula eu. Curabitur luctus ut ligula a iaculis. Aliquam erat volutpat. Pellentesque magna nibh, aliquam sit amet consectetur eget, auctor quis neque.',
@@ -40,3 +64,5 @@ let formArticle = {
 }
 </script>
 
+<style>
+</style>
