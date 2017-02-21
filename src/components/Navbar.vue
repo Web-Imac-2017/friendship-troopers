e2ed3f57c49a8aa5e946aab0e39b7cab312f59bf
PrincessDu31 <elise.ritoux@orange.fr>
2017-02-21 15:00:38 +0100

    navbar all item and icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,8 +9,11 @@
 
       <li class="navbar-right">
         <input type ="text" class="nav-item" placeholder="Recherche" value =""></input>
-        <router-link to="/utilisateur/amis" class="nav-item nav-link"> Amis </router-link>
-        <router-link to="/utilisateur/conversations" class="nav-item nav-link"> Messages persos </router-link>
+        <input type="submit" class="nav-search-icon" value=""></input>
+        
+        <router-link to="/utilisateur/amis" class="nav-item nav-link"> <img src="../assets/Friends.svg"> </router-link>
+        <router-link to="/utilisateur/conversations" class="nav-item nav-link"> <img src="../assets/Messages.svg"> </router-link>
+        <div class="nav-item nav-link"> <img src="../assets/Notifications.svg"> </div>
         <router-link to="/utilisateur" class="nav-item nav-link"> Mon profil </router-link>
       </li>
       <li>&nbsp;&nbsp;&nbsp;&nbsp;</li>
