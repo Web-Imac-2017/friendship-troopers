c81e526f21cd013d77134b873e050659b879a422
PrincessDu31 <elise.ritoux@orange.fr>
2017-02-21 13:40:00 +0100

    orga navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,17 @@
 <template>
   <nav class="navbar">
-    <ul class="nav navbar-nav navbar-right">
-      <li>
-        <a class="navbar-brand" href="/"> <!-- {{$store.state.appTitle}} {{$store.state.appVersion}} -->Friendship Troopers</a>
+    <ul class="nav navbar-nav">
+      <li class="navbar-left">
+        <a class="navbar-brand" href="/"> <!-- {{$store.state.appTitle}} {{$store.state.appVersion}} -->Friendship<br/>Troopers</a>
         <router-link to="/planete/actualites" class="nav-item nav-link"> Accueil </router-link>
         <router-link to="/utilisateur/enigmes" class="nav-item nav-link"> Énigmes </router-link>
+      </li>
+
+      <li class="navbar-right">
         <input type ="text" class="nav-item" placeholder="Recherche" value =""></input>
-        <router-link to="/" class="nav-item nav-link"> Login </router-link>
-        <router-link to="/inscription" class="nav-item nav-link"> Inscription </router-link>
+        <router-link to="/utilisateur/amis" class="nav-item nav-link"> Amis </router-link>
+        <router-link to="/utilisateur/conversations" class="nav-item nav-link"> Messages persos </router-link>
+        <router-link to="/utilisateur" class="nav-item nav-link"> Mon profil </router-link>
       </li>
       <li>&nbsp;&nbsp;&nbsp;&nbsp;</li>
     </ul>
