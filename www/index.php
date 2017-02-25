b61df33dea0a54fceee5df5db9bdd9f96749e1d5
Lila Gazeau <lilagazeau@gmail.com>
2017-02-25 10:11:47 +0100

    router V1

--- www/index.php
+++ www/index.php
@@ -6,10 +6,22 @@ require_once ROOT.'/api/utils/Autoloader.php';
 
 \Utils\Autoloader::register();
 \Utils\Config::init();
+// j'ai mis en get, mais il est possible que vous ayez besoin de post pour certaines routes.
+//Il faudra que vous le changiez
+//Vous aurez également à en ajouter
+$router = new Router($_GET['url']);
+$router->get('/', function(){ echo "homepage !"; });
+$router->get('/inscription', 'test');
+$router->get('/connexion', '' );
+$router->get('/:username', 'Account#profil');
+$router->get('/:planet/posts/:id', 'Publication#view');
+$router->get('/planet/wiki', 'Planet#wiki');
+$router->get('/planets', 'Planet#getAllPlanet');
+$router->get('/univers', '');
 
-//var_dump(\Utils\Config::get('db.hostname'));
-
-$model = new \Models\Model();
-$controller = new \Controllers\publication();
-$controller->view(1);
-//$controller = new SignIn();
+$router->run();
+//COMMENT CREER UNE ROUTE :
+//EN GET :
+// $router->get('/posts/:id', 'CONTROLLER#METHOD' });
+//EN POST :
+// $router->post('/posts/:id', 'CONTROLLER#METHOD' });
