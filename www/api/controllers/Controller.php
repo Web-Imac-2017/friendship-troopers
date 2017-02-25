4457858984c29ebad42700ddd59291e85ee9ad3c
Gaelle <calmels.gaelle@gmail.com>
2017-02-25 20:05:06 +0100

    fonctions de publication

--- www/api/controllers/Controller.php
+++ www/api/controllers/Controller.php
@@ -7,6 +7,26 @@
 namespace Controllers;
 
 abstract class Controller {
+  
+  public function filterXSS ($data) {
+    $currentData = $data;
+    if (!isset($currentData))
+      return -1;
+
+    if (!is_array($currentData)) {
+      $currentData = htmlspecialchars($currentData);
+    } else {
+      foreach ($currentData as &$value) {
+        $value = htmlspecialchars($value);
+      }
+      unset($value);
+    }
+    return $currentData;
+  }
+
+  public function filterCSRF () {
+
+  }
 
   public function filterXSS ($data) {
     $currentData = $data;
