d2bfe9d81c2829309a294eda7561b55a4ad9ba11
Gaelle <calmels.gaelle@gmail.com>
2017-02-25 13:23:57 +0100

    remise a jour de dev

--- www/api/controllers/Publication.php
+++ www/api/controllers/Publication.php
@@ -8,10 +8,12 @@ namespace Controllers;
 
 class Publication extends Controller {
 
-  public function view($id) {
+  public function view($title) {
     $model = new \Models\Publication();
-    $request = $model->findFirst(['fields' => ['title', 'content'],
-                          'conditions' => ['id', $id]]);
+    $request = $model->findFirst([
+      'fields' => ['title', 'content', 'publishDate', 'userId'],
+      'conditions' => ['title' => $title]
+    ]);
     var_dump($request);
   }
 }
--- www/index.php
+++ www/index.php
@@ -1,14 +1,14 @@
 <?php
-
 define('ROOT', __DIR__);
 
 require_once ROOT.'/api/utils/Autoloader.php';
-
 \Utils\Autoloader::register();
 \Utils\Config::init();
+
 // j'ai mis en get, mais il est possible que vous ayez besoin de post pour certaines routes.
-//Il faudra que vous le changiez
-//Vous aurez également à en ajouter
+// Il faudra que vous le changiez
+// Vous aurez également à en ajouter
+
 $router = new Router($_GET['url']);
 $router->get('/', function(){ echo "homepage !"; });
 $router->get('/inscription', 'test');
@@ -18,7 +18,6 @@ $router->get('/:planet/posts/:id', 'Publication#view');
 $router->get('/planet/wiki', 'Planet#wiki');
 $router->get('/planets', 'Planet#getAllPlanet');
 $router->get('/univers', '');
-
 $router->run();
 //COMMENT CREER UNE ROUTE :
 //EN GET :
