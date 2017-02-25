967f91783b41c7dd24e0fb9d81acbb17788ee962
Gaelle <calmels.gaelle@gmail.com>
2017-02-25 20:14:37 +0100

    modif de index.php

--- www/index.php
+++ www/index.php
@@ -1,15 +1,20 @@
 <?php
-
 define('ROOT', __DIR__);
 
-require_once ROOT.'/api/utils/Autoloader.php';
+if ($_GET['url'] === 'favicon.ico') {
+  http_response_code(404);
+  exit();
+}
 
+require_once ROOT.'/api/utils/Autoloader.php';
 \Utils\Autoloader::register();
 \Utils\Config::init();
+
 // j'ai mis en get, mais il est possible que vous ayez besoin de post pour certaines routes.
-//Il faudra que vous le changiez
-//Vous aurez également à en ajouter
-$router = new Router($_GET['url']);
+// Il faudra que vous le changiez
+// Vous aurez également à en ajouter
+
+$router = new Utils\Router\Router($_GET['url']);
 $router->get('/', function(){ echo "homepage !"; });
 $router->get('/inscription', 'test');
 $router->get('/connexion', '' );
@@ -18,10 +23,19 @@ $router->get('/:planet/posts/:id', 'Publication#view');
 $router->get('/planet/wiki', 'Planet#wiki');
 $router->get('/planets', 'Planet#getAllPlanet');
 $router->get('/univers', '');
-
 $router->run();
+
 //COMMENT CREER UNE ROUTE :
 //EN GET :
 // $router->get('/posts/:id', 'CONTROLLER#METHOD' });
 //EN POST :
 // $router->post('/posts/:id', 'CONTROLLER#METHOD' });
+
+//session_start();
+//$userId = $_SESSION['user'] = new Models\user(['id' => 1]);
+$publication = new \Controllers\Publication();
+$title = 'Gaëlle';
+$content = 'super content';
+//$publication->create($title, $content, 1);
+//$publication->delete(1, 1);
+//$publication->update($title, $content, 1);
