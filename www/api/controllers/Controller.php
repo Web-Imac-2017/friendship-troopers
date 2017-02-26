4ee05c782678ea648819486d4e0785a5a17c31f5
Gaelle <calmels.gaelle@gmail.com>
2017-02-26 11:53:30 +0100

    resolutio conflits

--- www/api/controllers/Controller.php
+++ www/api/controllers/Controller.php
@@ -27,27 +27,4 @@ abstract class Controller {
   public function filterCSRF () {
 
   }
-
-<<<<<<< HEAD
-  public function filterXSS ($data) {
-    $currentData = $data;
-    if (!isset($currentData))
-      return -1;
-
-    if (!is_array($currentData)) {
-      $currentData = htmlspecialchars($currentData);
-    } else {
-      foreach ($currentData as &$value) {
-        $value = htmlspecialchars($value);
-      }
-      unset($value);
-    }
-    return $currentData;
-  }
-
-  public function filterCSRF () {
-
-  }
-=======
->>>>>>> 4bc30704667d8cda7f042eaf3b483f95fcd33a62
 }
