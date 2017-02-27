57306e3ca6a3e2ffdaca571c5b48e52d06eb0f07
Luckypon <marion.vila.25@live.fr>
2017-02-27 19:57:02 +0100

    Continued the login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,9 @@
 <template>
 	<div class="container login">
 
-
 	  	<div class="row">
 	  		<div class="col-sm-4">
-	  			<h1>Frienship troopers</h1>
+	  			<h1>Friendship troopers</h1>
 	  			<div class="center">
 	  				<p>
 	  					Venez partager votre passion de la science-fiction à travers ce réseau social mélant énigmes et bonne humeur !
@@ -16,16 +15,19 @@
 	  			</div>
 	  		</div>
 	  		<div class="col-sm-8">
+	  			<div class="row">
+	  				<div class="col-sm-5" @click="inscription"><h1>Inscription</h1></div>
+	  				<div class="col-sm-5" @click="connexion"><h1>Connexion</h1></div>
+	  			</div>
 
-				<form-user v-model="user" title="Request form">
+				<form-user :user="user" :login="login" title="Request form">
 
 					<div v-show='error1' slot="error1">{{user.username}} est déjà utilisé !</div>
 					<div v-show='error2' slot="error2">L'adresse mail {{user.mail}} est déjà utilisée !</div>
 
 				</form-user>
-				<!-- <img src="../../www/assets/images/FuseeFirstPage.svg" alt="fusée"> -->
 
-				<pre>{{user}}</pre>
+				<!-- <pre>{{user}}</pre> -->
 
 			</div>
 
@@ -37,63 +39,103 @@
 
 <script>
 let formUser = {
-	props:{
-		value:Object
-	},
-	data(){
-		return{
-			user:JSON.parse(JSON.stringify(this.value)),
-			title: 'Inscription',
-		}
-	},
+	props: ['login','user'],
 	methods:{
 		save(){
 			this.$emit("input", this.user),
 			this.error1=true,
-        	this.error2=true
-			
+        	this.error2=true	
 		}
 	},
 	template: `
-	<form class="form" @submit.prevent="save">
-		<h1>{{ title }}</h1>
-		
-		<div class="field">
-			<label for="">Pseudo</label>
-			<input type="text" v-model="user.username" name='username'>
-		</div>
-		<div class="field">
-			<label for="">Mail</label>
-			<input type="email" v-model="user.mail" name='mail'>
-		</div>
-		<div class="field">
-			<label for="">Mot de passe</label>
-			<input type="password" v-model="user.password" name='password'>
-		</div>
-		<div class="field">
-			<a href="">J'ai déjà un compte</a>
-			<button v-on:click="save">S'inscrire</button>
-		</div>
-		<p><slot name="error1"></slot></p>
+	<div>
+		<form v-if="login" class="form" @submit.prevent="save">
+			<div class="field">
+				<label for="">Pseudo</label>
+				<input type="text" v-model="user.username" name='username'>
+			</div>
+			<div class="field">
+				<label for="">Adresse e-mail</label>
+				<input type="email" v-model="user.mail" name='mail'>
+			</div>
+			<div class="field" >
+				<label for="">Mot de passe</label>
+				<input type="password" v-model="user.password" name='password'>
+			</div>	
+			<div class="field">
+				<button v-on:click.stop="save">Se connecter<img class="img-rocket" src="static/FuseeFirstPage.svg"> </button>
+			</div>
+	    </form>
+	    <form v-else class="form" @submit.prevent="save">
+			<div class="field">
+				<label for="">Pseudo</label>
+				<input type="text" v-model="user.username" name='username'>
+			</div>
+			<div class="field">
+				<label for="">Adresse e-mail</label>
+				<input type="email" v-model="user.mail" name='mail'>
+			</div>
+			<div class="field" >
+				<label for="">Mot de passe</label>
+				<input type="password" v-model="user.password" name='password'>
+			</div>
+			<div class="field">
+				<label for="">Mot de passe<br>(répeter)</label>
+				<input type="password" v-model="user.passwordChecked" name='password'>
+			</div>
+			<div class="field">
+				<label for="">Date de naissance</label>
+				<select name="day" id="day">
+			       <option v-for="option in user.day">
+			            {{ option }}
+			        </option>
+		       </select>
+		       <select  name="month" id="month">
+			       <option v-for="option in user.month">
+			            {{ option }}
+			        </option>
+		       </select>
+		       <select name="year" id="year">
+			       <option v-for="option in user.year">
+			            {{ option }}
+			        </option>
+		       </select>
+			</div>
+			<div class="field">
+				<button v-on:click.stop="save">S''inscrire<img class="img-rocket" src="static/FuseeFirstPage.svg"> </button>
+			</div>
+	    </form>
+    	<p><slot name="error1"></slot></p>
 		<p><slot name="error2"></slot></p>
-    </form>
+	</div>
+	
 	  		
 	`
-	,/*mounted: function(){
-		console.log(this)
-	}*/
 }
 export default {
 	components: {formUser },
+	methods: {
+	    inscription: function(){
+	      this.login = false
+	    },
+	    connexion: function(){
+	      this.login = true
+	    }
+	  }, 
 	data () {
       return {
         user:{
         	username: '',
 	        mail: '',
 	        password: '',
+	        passwordChecked: '',
+	        day : [ 1, 2, 3],
+	        month : [ 'janvier', 'fevrier', 'mars'],
+	        year : [ 1995, 1996, 2874]
         },
         error1:false,
-        error2:false
+        error2:false,
+        login:false
       }
     }
 }
