95928c4858caaa5b3efb693d741af704a8cc2d3d
nornei <noemie.pije@gmail.com>
2017-02-27 18:24:36 +0100

    return 0; return       return 0;0;

---
+++ src/components/PageA.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>PageA</div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
--- www/api/models/Planet.php
+++ www/api/models/Planet.php
@@ -7,5 +7,44 @@
 namespace Models;
 
 class Planet extends Model {
-	
-}
+	// ----------[ CONSTANTS ]----------
+
+	// ----------[ ATTRIBUTS ]----------
+	private $id;
+	private $name;
+	private $description;
+	private $history;
+	private $imagePath;
+
+// //----------[ CONSTRUCT ]----------
+// function __construct($params = false) {
+// 	parent::__construct();
+// 	 if (is_array($params)) {
+// 		 foreach ($params as $key => $val) {
+// 			 if($key != "password") {
+// 				 $this->$key = $val;
+// 			 }
+// 		 }
+// 	 }
+// }
+//
+// //----------[ SETTER ]-------------
+//  public function __set($attr, $value) {
+// 	 $func = 'set'.ucfirst($attr);
+// 	 if (method_exists($this, $func)) {
+// 		  $this->$func($value);
+// 		} else {
+// 			$this->$attr = $value;
+// 		}
+// 	 // Action commune à tous les setters (ex : mettre dans l'objet)
+//  }
+//
+//  //----------[ GETTER ]-------------
+//   public function __get($attr) {
+// 		if(isset($this->$attr)) {
+// 			return $this->$attr;
+// 		} else {
+// 			return 0;
+// 		}
+	}
+?>
