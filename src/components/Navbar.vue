e239818c30c6aeeecc5db8df43e050cb85481016
PrincessDu31 <elise.ritoux@orange.fr>
2017-02-28 21:32:47 +0100

    submenus navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav class="navbar">
+  <div class="navbar row">
     <ul class="nav navbar-nav">
       <li class="navbar-left">
         <a class="navbar-brand" href="/"> <!-- {{$store.state.appTitle}} {{$store.state.appVersion}} -->Friendship<br/>Troopers</a>
         <menu-separator></menu-separator>
-        <router-link to="/actualites" class="nav-item nav-link"> Accueil </router-link>
+        <router-link to="/actualites" class="nav-item nav-link nav-home"> Accueil </router-link>
         <router-link to="/utilisateur/enigmes" class="nav-item nav-link"> <b>Énigmes</b> </router-link>
       </li>
 
@@ -13,25 +13,93 @@
         <input type="submit" class="nav-item nav-search-icon" value=""></input>
         
         <menu-separator></menu-separator>
-        <div class="nav-item nav-icon friends-icon"> 
-          <p v-if="notif.friends > 99" class="notif">99+</p> 
-          <p v-else-if="notif.friends > 0" class="notif"> {{ notif.friends }} </p> 
+        
+        <div class="nav-item nav-icon " @click="showSubMenu(1)">
+          <div class="friends-icon">
+          <p v-if="friends.length > 99" class="notif">99+</p> 
+          <p v-else-if="friends.length > 0" class="notif"> {{ friends.length }} </p> 
         </div>
-        <div class="nav-item nav-icon messages-icon"> 
-          <p v-if="notif.messages > 99" class="notif">99+</p>
-          <p v-else-if="notif.messages > 0" class="notif"> {{ notif.messages }}</p> 
-        </div>
-        <div class="nav-item nav-icon notifications-icon">
-          <p v-if="notif.notifications > 99" class="notif">99+</p> 
-          <p v-else-if="notif.notifications > 0" class="notif"> {{ notif.notifications }}</p> 
+          <div v-if="subMenu == 1" class="sub-menu"> 
+            <ul class="menu-friends">
+            <p v-if = "friends.length > 0">
+              Vous avez des demandes d'amis ! <br/>
+            </p>
+            <p v-else>
+              Vous n'avez pas de nouvelles demandes d'ami... <br/>
+            </p>
+
+              <li v-if = "friends.length > 0">
+                {{friends[0].pseudo}} vous a invité le {{friends[0].date}}
+              </li>
+              <li v-if = "friends.length > 1">
+                {{friends[1].pseudo}} vous a invité le {{friends[1].date}}
+              </li>
+
+            </ul>
+          </div>
+
         </div>
 
+
+        <div class="nav-item nav-icon" @click="showSubMenu(2)"> 
+          <div class="messages-icon">
+            <p v-if="messages.length > 99" class="notif">99+</p>
+            <p v-else-if="messages.length > 0" class="notif"> {{ messages.length }}</p>
+          </div>
+
+          <div v-if="subMenu == 2" class="sub-menu"> 
+            <ul class="menu-messages">
+
+              <p v-if = "messages.length > 0">
+               Vous avez des nouveaux messages ! <br/>
+              </p>
+              <p v-else>
+                Vous n'avez pas de nouveaux messages... <br/>
+              </p>
+
+              <li v-if = "messages.length > 0">
+                {{messages[0].pseudo}}, le {{messages[0].date}}
+              </li>
+              <li v-if = "messages.length > 1">
+                {{messages[1].pseudo}}, le {{messages[1].date}}
+              </li>
+            </ul>
+          </div> 
+
+        </div>
+        
+
+        <div class="nav-item nav-icon" @click="showSubMenu(3)">
+          <div class="notifications-icon">
+              <p v-if="notifications.length > 99" class="notif">99+</p> 
+              <p v-else-if="notifications.length > 0" class="notif"> {{ notifications.length }}</p> 
+         </div>
+
+          <div v-if="subMenu == 3" class="sub-menu"> 
+            <ul class="menu-notifications">
+
+              <p v-if = "notifications.length == 0">
+               Vous n'avez pas nouvelles notifications... <br/>
+              </p>
+
+              <li v-if = "notifications.length > 0">
+                {{notifications[0].pseudo}} a {{notifications[0].type}} votre post {{notifications[0].post}}
+              </li>
+              <li v-if = "notifications.length > 1">
+                {{notifications[1].pseudo}} a {{notifications[1].type}} votre post {{notifications[1].post}}
+              </li>
+            </ul>
+          </div>
+     
+        </div>
+
+
         <menu-separator></menu-separator>
-        <router-link to="/utilisateur" class="nav-item nav-link"> Mon profil </router-link>
-        <div class="nav-responsive nav-item nav-link"  @click="showMenuPesponsive()"> <img src="../assets/MenuResponsive.svg"> </div>
+        <router-link to="/utilisateur" class="nav-item nav-link nav-profil"> Mon profil </router-link>
+
       </li>
     </ul>
-  </nav>
+  </div>
 </template>
 
 <style src="../../www/assets/css/navbar.css"></style>
@@ -42,25 +110,53 @@
 export default {
   methods: {
     showMenuPesponsive: function(){
-      
+    }, 
+    showSubMenu: function(idMenu){
+      if (this.subMenu == idMenu) 
+        this.subMenu = 0
+      else {
+        this.subMenu = idMenu
+      }
     }, 
     showNotifications: function(){
-      
+      for (var i = 0; i < this.friends.length; i++) {
+       // alert(this.friends[i].pseudo);
+     }
     }, 
     showMessages: function(){
-      
+      if (this.subMenu == 2) 
+        this.subMenu = 0
+      else 
+        this.subMenu = 2
     }, 
     showFriends: function(){
-      
+      if (this.subMenu == 1) 
+        this.subMenu = 0
+      else 
+        this.subMenu = 1
     }
   }, 
    data () {
       return {
-        notif:{
-          friends : 1,
-          messages : 16,
-          notifications : 100
-        }
+        friends: [ {pseudo: 'luckypon',date : '12/03/13'},
+                    {pseudo: 'luckyLuke',date : '12/03/13'}
+                 ],
+        messages: [ {pseudo: 'luckypon',date : '12/03/13'},
+                    {pseudo: 'tintin',date : '11/02/12'},
+                    {pseudo: 'kirikoukou',date : '01/03/13'}
+                 ], 
+        notifications: [
+                    {type:'like', post:'4', pseudo:'luckypon'},
+                    {type:'comment', post:'2', pseudo: 'hihi'}     
+        ], 
+        subMenu : 0
+
+        // notifs:[{
+        //   friends : 1,
+        //   messages : 16,
+        //   notifications : 100
+        // }], 
+        // subMenu : 0 /*1:friends / 2: messages / 3: notif*/
       }
     }
 
