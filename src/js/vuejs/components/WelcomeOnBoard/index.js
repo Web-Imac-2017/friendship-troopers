d3f005c4dc07cc7752caff476e330ff51547524e
JulieCapucine <jujucapucine.jp@gmail.com>
2017-03-03 21:37:38 +0100

    added images + correction and style to Welcome on Board

--- src/js/vuejs/components/WelcomeOnBoard/index.js
+++ src/js/vuejs/components/WelcomeOnBoard/index.js
@@ -55,46 +55,46 @@ const WelcomeOnBoard = Vue.extend({
        questions: [ {
          number : "1",
          title : 'Ta couleur : ' ,
-         answer : [ {text: 'bleu',image : "../../../www/assets/images/Avatar1.svg", planetId : '1'},
-                    {text:'vert', image : "../../../www/assets/images/Avatar1.svg", planetId : '2'},
-                    {text:'orange',image : "../../../www/assets/images/Avatar1.svg", planetId : '3'},
-                    {text:'violet', image : "../../../www/assets/images/Avatar1.svg", planetId : '4'},
-                    {text:'noir', image : "../../../www/assets/images/Avatar1.svg", planetId : '5'}
+         answer : [ {text: 'bleu',image : "../../../www/assets/images/welcomeOnBoard/1_planete-bleue.jpg", planetId : '1'},
+                    {text:'vert', image : "../../../www/assets/images/welcomeOnBoard/1_planete-verte.jpg", planetId : '2'},
+                    {text:'orange',image : "../../../www/assets/images/welcomeOnBoard/1_planete-violette.jpg", planetId : '3'},
+                    {text:'violet', image : "../../../www/assets/images/welcomeOnBoard/1_planete-rouge.jpg", planetId : '4'},
+                    {text:'noir', image : "../../../www/assets/images/welcomeOnBoard/1_planete-orange.jpg", planetId : '5'}
          ]
         }, {
           number : "2",
           title : 'Ton compagnon :',
-          answer : [ {text:'un petit vélociraptor', image : "../../../www/assets/images/Avatar1.svg", planetId : '1'},
-                     {text:'un mini Groot', image : "../../../www/assets/images/Avatar1.svg", planetId : '2'}, 
-                     {text:'une charmante androïde', image : "../../../www/assets/images/Avatar1.svg", planetId : '3'}, 
-                     {text:'Doc', image : "../../../www/assets/images/Avatar1.svg", planetId : '4'}, 
-                     {text:'un Choubacca', image : "../../../www/assets/images/Avatar1.svg", planetId : '5'} 
+          answer : [ {text:'un petit vélociraptor', image : "../../../www/assets/images/welcomeOnBoard/2_velociraptor.jpg", planetId : '1'},
+                     {text:'un mini Groot', image : "../../../www/assets/images/welcomeOnBoard/2_groot.png", planetId : '2'}, 
+                     {text:'une charmante androïde', image : "../../../www/assets/images/welcomeOnBoard/2_android.png", planetId : '3'}, 
+                     {text:'Doc', image : "../../../www/assets/images/welcomeOnBoard/2_Doc.jpg", planetId : '4'}, 
+                     {text:'un Choubacca', image : "../../../www/assets/images/welcomeOnBoard/2_choubacca.jpeg", planetId : '5'} 
          ] 
         }, {
           number : "3",
           title : 'Tes vacances parfaites :' ,
-          answer : [ {text:'un week-end romantique à New New York', image : "../../../www/assets/images/Avatar1.svg", planetId : '1'},
-                     {text:'tester les spécialités culinaires de Vulcain', image : "../../../www/assets/images/Avatar1.svg", planetId : '2'},
-                     {text:'relire tes classiques', image : "../../../www/assets/images/Avatar1.svg", planetId : '3'}, 
-                     {text:'arpenter un trou noir', image : "../../../www/assets/images/Avatar1.svg", planetId : '4'},
-                     {text:'faire la course de la Bounta', image : "../../../www/assets/images/Avatar1.svg", planetId : '5'}
+          answer : [ {text:'un week-end romantique à New New York', image : "../../../www/assets/images/welcomeOnBoard/3_new-new-york.jpg", planetId : '1'},
+                     {text:'tester les spécialités culinaires de Vulcain', image : "../../../www/assets/images/welcomeOnBoard/3_vulcain.jpg", planetId : '2'},
+                     {text:'relire tes classiques', image : "../../../www/assets/images/welcomeOnBoard/3_lire-classiques.jpg", planetId : '3'}, 
+                     {text:'arpenter un trou noir', image : "../../../www/assets/images/welcomeOnBoard/3_trous-noirs.jpg", planetId : '4'},
+                     {text:'faire la course de la Bounta', image : "../../../www/assets/images/welcomeOnBoard/3_bounta.jpg", planetId : '5'}
          ] 
         },{
           number : "4",
           title : 'L\'intelligence artificielle pour gérer ta maison :' ,
-          answer : [{text: 'la Matrice', image : "../../../www/assets/images/Avatar1.svg", planetId : '1'}, 
-                    {text:'le Brain Bug', image : "../../../www/assets/images/Avatar1.svg", planetId : '2'}, 
-                    {text:'GladOS', image : "../../../www/assets/images/Avatar1.svg", planetId : '3'}, 
-                    {text:'AI du HIVE', image : "../../../www/assets/images/Avatar1.svg", planetId : '4'}, 
-                   {text:'Hall', image : "../../../www/assets/images/Avatar1.svg", planetId : '5'}
+          answer : [{text: 'la Matrice', image : "../../../www/assets/images/welcomeOnBoard/4_matrice.jpg", planetId : '1'}, 
+                    {text:'le Brain Bug', image : "../../../www/assets/images/welcomeOnBoard/4_brainBug.jpg", planetId : '2'}, 
+                    {text:'GladOS', image : "../../../www/assets/images/welcomeOnBoard/4_Glados.jpg", planetId : '3'}, 
+                    {text:'La Reine Rouge', image : "../../../www/assets/images/welcomeOnBoard/4_red-queen.png", planetId : '4'}, 
+                   {text:'AUTO', image : "../../../www/assets/images/welcomeOnBoard/4_AUTO.jpg", planetId : '5'}
          ]},{
           number : "5",
           title : 'Ton moyen de transport de prédilection :' , 
-          answer : [ {text:'le TARDIS', image : "../../../www/assets/images/Avatar1.svg", planetId : '1'},
-                     {text:'la Porte des Etoiles', image : "../../../www/assets/images/Avatar1.svg", planetId : '2'},
-                     {text:'l\'astro-stop', image : "../../../www/assets/images/Avatar1.svg", planetId : '3'},
-                     {text:'la DeLorean', image : "../../../www/assets/images/Avatar1.svg", planetId : '4'}, 
-                    {text:'l\'Arcadia', image : "../../../www/assets/images/Avatar1.svg", planetId : '5'}
+          answer : [ {text:'le TARDIS', image : "../../../www/assets/images/welcomeOnBoard/5_tardis.jpg", planetId : '1'},
+                     {text:'la Porte des Etoiles', image : "../../../www/assets/images/welcomeOnBoard/5_porte-des-etoiles.jpg", planetId : '2'},
+                     {text:'l\'astro-stop', image : "../../../www/assets/images/welcomeOnBoard/5_astro-stop.jpg", planetId : '3'},
+                     {text:'la DeLorean', image : "../../../www/assets/images/welcomeOnBoard/5_deLorean.jpg", planetId : '4'}, 
+                    {text:'l\'Arcadia', image : "../../../www/assets/images/welcomeOnBoard/5_arcadia.jpg", planetId : '5'}
          ] 
         },{
           number : "6",
