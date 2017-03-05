31cecdad6a73e8cbdeaddcbbd6fc7aabfaea8854
JulieCapucine <jujucapucine.jp@gmail.com>
2017-03-05 17:54:23 +0100

    added some style and components from new design

--- src/js/vuejs/components/Feed/index.js
+++ src/js/vuejs/components/Feed/index.js
@@ -11,6 +11,7 @@ import LateralMenuRight from '../LateralMenuRight/index.js'
 import MenuTools from '../MenuTools/index.js'
 import Post from '../Post/index.js'
 import CreatePost from '../CreatePost/index.js'
+import PageNav from './pageNav/index.js'
 
 
 const Feed = Vue.extend({
@@ -20,19 +21,70 @@ const Feed = Vue.extend({
   	'lateral-menu-left' : LateralMenuLeft,
   	'lateral-menu-right' : LateralMenuRight, 
   	'menu-tools' : MenuTools, 
-  	'post' : Post 
+  	'post' : Post ,
+    'page-nav' : PageNav
   },
+ 
   	data () {
       	return {
-      		post : {
+      		posts : [{
         	  user: 'Lucky',
-	          avatar : "addresse de l'avatar",
+	          avatar : "../assets/images/Avatar1.svg",
+            planeteId : 2,
 	          date: '20 fev',
 	          hour: '12h04',
 	          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tortor eu ipsum laoreet faucibus. Etiam mattis eros id leo maximus blandit. Proin id massa in risus gravida suscipit non eu arcu. Aenean auctor lacus risus, porttitor sodales odio vehicula eu. Curabitur luctus ut ligula a iaculis. Aliquam erat volutpat. Pellentesque magna nibh, aliquam sit amet consectetur eget, auctor quis neque.',
-	          likes: '50',
-	          comments: '2'
-	        }
+	          likes: 50,
+	          comments: [
+            {
+              user : "Moi",
+              content : "la vie c'est du kiri"
+            }, {
+              user : "Toi",
+              content : "Non, la vie c'est du kiwi"
+            }
+            ],
+            id : 1
+	        },
+          {
+            user: 'Lucky',
+            avatar : "../assets/images/Avatar1.svg",
+            planeteId : 3,
+            date: '20 fev',
+            hour: '12h04',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tortor eu ipsum laoreet faucibus. Etiam mattis eros id leo maximus blandit. Proin id massa in risus gravida suscipit non eu arcu. Aenean auctor lacus risus, porttitor sodales odio vehicula eu. Curabitur luctus ut ligula a iaculis. Aliquam erat volutpat. Pellentesque magna nibh, aliquam sit amet consectetur eget, auctor quis neque.',
+            likes: 50,
+            comments: [
+            {
+              user : "Moi",
+              content : "la vie c'est du kiri"
+            }, {
+              user : "Toi",
+              content : "Non, la vie c'est du kiwi"
+            }
+            ],
+            id : 2
+          },
+          {
+            user: 'Lucky',
+            avatar : "../assets/images/Avatar1.svg",
+            planeteId : 4,
+            date: '20 fev',
+            hour: '12h04',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tortor eu ipsum laoreet faucibus. Etiam mattis eros id leo maximus blandit. Proin id massa in risus gravida suscipit non eu arcu. Aenean auctor lacus risus, porttitor sodales odio vehicula eu. Curabitur luctus ut ligula a iaculis. Aliquam erat volutpat. Pellentesque magna nibh, aliquam sit amet consectetur eget, auctor quis neque.',
+            likes: 50,
+            comments: [
+            {
+              user : "Moi",
+              content : "la vie c'est du kiri"
+            }, {
+              user : "Toi",
+              content : "Non, la vie c'est du kiwi"
+            }
+            ],
+            id : 3
+          }
+          ]
         }
   	}
 
--- src/js/vuejs/components/Post/index.js
+++ src/js/vuejs/components/Post/index.js
@@ -1,15 +1,37 @@
 'use strict';
 
 import Vue from 'vue/dist/vue';
-import {apiRoot} from '../../../../../config.js'
+// import {apiRoot} from '../../../../../config.js'
 
 let template = require('./template.html');
 template     = eval(`\`${template}\``);
 
 
 const Post = Vue.extend({
+  props : {
+    post:Object
+  },
   template,
-  created () {
+   methods : {
+    like : function(){
+      this.post.likes += 1;
+      //TODO dans requete ajax, post like, id post + user name (get from session)
+    },
+    publishNewComment : function() {
+      console.log(this.newComment);
+    }
+  },
+
+  data () {
+    return {
+      newComment : '',
+      planetData: ["#3eb6df", "#ef4646", "#767fe2", "#72b51a", "#f9a519" ]
+
+       
+
+    }
+  }
+  // created () {
    /* this.$http.get(apiRoot() +'/univers/Terre/actualites').then(
         (response) => {
           console.log("ok "+response.body);
@@ -19,42 +41,7 @@ const Post = Vue.extend({
           console.log("fail " + JSON.stringify(response.data));
         }
       )*/
-  },
-  data () {
-      return {
-        post:{
-          user: 'Lucky',
-          avatar : "addresse de l'avatar",
-          date: '20 fev',
-          hour: '12h04',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut tortor eu ipsum laoreet faucibus. Etiam mattis eros id leo maximus blandit. Proin id massa in risus gravida suscipit non eu arcu. Aenean auctor lacus risus, porttitor sodales odio vehicula eu. Curabitur luctus ut ligula a iaculis. Aliquam erat volutpat. Pellentesque magna nibh, aliquam sit amet consectetur eget, auctor quis neque.',
-          likes: '50',
-          comments: '2'
-        }, 
-        picture:false,
-        actualites: [], 
-        user : {
-          userAvatar : '../assets/logo.png',
-          username : 'LuckyPon', 
-          userBadge : 'Baroudeuse de l\'espace', 
-          userPlanet : {
-            path : '../assets/Avatar1.svg',
-            name : 'Planete X785-E'
-          }, 
-          lastFriends : [ {
-            avatar : '../assets/Avatar1.svg',
-            name : 'Spockdu77'
-          }, {
-            avatar : '../assets/Avatar1.svg',
-            name : 'MartyDuPassé'
-          }, {
-            avatar : '../assets/Avatar1.svg',
-            name : 'JulieDuFutur'
-          }
-          ]
-        }
-      }
-    }
+  // }
 });
 
 
