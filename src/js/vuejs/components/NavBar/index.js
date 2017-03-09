9cb591825078e72d274ae46eedd4d4783d445e5b
PrincessDu31 <elise.ritoux@orange.fr>
2017-03-09 18:20:37 +0100

    responsive search and add friends submenu

--- src/js/vuejs/components/NavBar/index.js
+++ src/js/vuejs/components/NavBar/index.js
@@ -14,14 +14,19 @@ Vue.component('friend-item', {
           <img :src=friend.avatar alt="avatar" class="avatar"> 
         </li>
         <li>
-          <p :class=friend.planet class="pseudo">{{friend.pseudo }}</p> 
-          <p class="date"> {{friend.date}}</p>
+          <p :class=friend.planet class="pseudo">{{friend.pseudo}}</p>
+          <div  class="friend-validation" > 
+            <img src="/assets/images/validation.svg" alt="valider" title="Valider">
+            <img src="/assets/images/Cross.svg" alt="refuser" title="Refuser">
+          </div>
         </li>
       </ul>
+
     </li>
     `
 
 })
+
 Vue.component('message-item', {
   props: ['message'],
   template:
@@ -31,11 +36,12 @@ Vue.component('message-item', {
           <img :src=message.avatar alt="avatar" class="avatar"> 
         </li>
         <li>
-          <p :class=message.planet class="pseudo">{{message.pseudo }}</p> 
+          <p :class=message.planet class="pseudo">{{message.pseudo}}</p> 
           <p class="date"> {{message.date}}</p>
         </li>
       </ul>
-      <p class="content"> {{message.begining}} </p>
+      <p class="content message" v-if="message.content.length>40" >  {{message.content.slice(0,20)}}...  </p>
+      <p class="content message" v-else>  {{message.content}}  </p>
     </li>`
 })
 
@@ -43,7 +49,7 @@ Vue.component('notification-item', {
   props: ['notification'],
   template:  
   `<li v-if = "notification.type==1"> 
-      <p>Ça y est ! Votre post est devenu galactique !</p>
+      <p class="content">Ça y est ! Votre post est devenu galactique !</p>
     </li>
     <li v-else-if = "notification.type==2"> 
       <ul class = "sub-menu-item">
@@ -51,7 +57,7 @@ Vue.component('notification-item', {
           <img :src=notification.avatar alt="avatar" class="avatar"> 
         </li>
         <li> 
-          <p>Votre ami {{notification.pseudo}} a publié un message !</p>
+          <p class="content">Votre ami {{notification.pseudo}} a publié un message !</p>
         </li>
       </ul>
     </li>
@@ -62,7 +68,7 @@ Vue.component('notification-item', {
           <img src="/assets/images/Messages.svg"> 
         </li>
         <li> 
-          <p>Votre dernier post fait réagir !</p>
+          <p class="content">Votre dernier post fait réagir !</p>
         </li>
       </ul>
     </li>`
@@ -73,7 +79,12 @@ Vue.component('notification-item', {
 const NavBar = Vue.extend({
   template,
     methods: {
-    showMenuResponsive: function(){
+    showSearch: function(){
+      if (this.search == 1) 
+        this.search = 0
+      else {
+        this.search = 1
+      }
     },
     showSubMenu: function(idMenu){
       if (this.subMenu == idMenu) 
@@ -85,14 +96,14 @@ const NavBar = Vue.extend({
   }, 
    data () {
       return {
-        friends: [ {pseudo: 'luckypon', avatar:'/assets/images/avatars/Terre/astro.svg', planet :'Terre',date : '12/03/13'},
-                    {pseudo: 'luckyLuke', avatar:'/assets/images/avatars/Sautien/astro.svg', planet :'Sautien', date : '12/03/13'},
-                    {pseudo: 'titi', avatar:'/assets/images/avatars/Technome/landscape.svg', planet :'Technome', date : '12/03/13'},
-                    {pseudo: 'tot', avatar:'/assets/images/avatars/Multas/astro.svg', planet :'Multas', date : '12/03/13'}
+        friends: [ {pseudo: 'luckypon', avatar:'/assets/images/avatars/Terre/astro.svg', planet :'Terre'},
+                    {pseudo: 'luckyLuke', avatar:'/assets/images/avatars/Sautien/astro.svg', planet :'Sautien'},
+                    {pseudo: 'titi', avatar:'/assets/images/avatars/Technome/landscape.svg', planet :'Technome'},
+                    {pseudo: 'tot', avatar:'/assets/images/avatars/Multas/astro.svg', planet :'Multas'}
                  ],
-        messages: [ {pseudo: 'luckypon',avatar:'/assets/images/avatars/Terre/astro.svg', planet :'earth', begining:"Je t'envoie un message toto !", date : '12/03/13'},
-                    {pseudo: 'tintin', avatar:'/assets/images/avatars/Multas/landscape.svg', planet :'Multas',begining:"Je t'envoie un message toto !", date : '11/02/12'},
-                    {pseudo: 'kirikoukou', avatar:'/assets/images/avatars/Multas/landscape.svg', planet :'Multas', begining:"Je t'envoie un message toto !", date : '01/03/13'}
+        messages: [ {pseudo: 'luckypon',avatar:'/assets/images/avatars/Terre/astro.svg', planet :'Terre', content:"Ok, ça marche !", date : '12/03/13'},
+                    {pseudo: 'tintin', avatar:'/assets/images/avatars/Multas/landscape.svg', planet :'Multas',content:"Je tor sit amet, consectetur adipisicing elit. Dicta soluta explicabo perferendis 'envoie un message toto !", date : '11/02/12'},
+                    {pseudo: 'kirikoukou', avatar:'/assets/images/avatars/Multas/landscape.svg', planet :'Multas', content:"Je t'envoie un message or sit amet, consectetur adipisicing elit. Dicta soluta explicabo perferendis tototariecot !", date : '01/03/13'}
                  ], 
         notifications: [
         /*  type 1 : post intergalactique / type 2 : ami news  /  type 3 : last post */
@@ -101,6 +112,7 @@ const NavBar = Vue.extend({
                     {type:'3', post:'3'}     
         ], 
         subMenu : 0,
+        search : 0,
         pseudo : 'luckypon', 
         avatar : '/assets/images/avatars/Multas/landscape.svg',
         deconnexion : '/assets/images/deconnexion.svg'
