54252d514b75a4487891ad2bb0aa3dad1d40a45a
Vanessa BAR <bar.vaness@gmail.com>
2017-03-11 18:09:22 +0100

    Link back and front, feed

--- src/js/vuejs/components/Post/index.js
+++ src/js/vuejs/components/Post/index.js
@@ -13,14 +13,14 @@ const Post = Vue.extend({
     'comment' : Comment
   },
   props : {
-    post: Object
+    post: Object,
+    new : Boolean
   },
   template,
    methods : {
     like : function() {
       this.$http.post(apiRoot() + "/posts/" + this.post.id + "/stardust").then(
         (response) => {
-          console.log("LIKE !");
           // On recupere le nouveau nombre de like
           //TODO
         },
@@ -28,69 +28,84 @@ const Post = Vue.extend({
         });
     },
     publishNewComment : function() {
-      console.log(this.newComment);
-      //TODO
+      this.$http.post(apiRoot() + "planets/1/posts/" + this.post.id + "/comments", { 'content' : this.newComment}, {emulateJSON : true}).then(
+        (response) => {
+          console.log("Comment created !");
+          this.newComment = '';
+        },(response) => {
+          console.log("Echec");
+        }
+      );
+      // TODO Actualiser les commentaires
     },
     loadMoreComment : function() {
-      this.loadMore += loadMore;
+      this.loadMore += this.loadMore;
+      
+      this.getComments(this.prevComments);
       //TODO
     },
     formateDate : function(date) {
       var object = new Date(date); 
       var months = ["janv.", "févr.", "mars", "avr.", "mai", "juin", "juill.", "août", "sept.", "oct.", "nov.", "déc."];
       return object.getDate() + " " + months[object.getMonth()] + " à " + object.getHours() + ":" + object.getMinutes();
-    }
-  },
-  created : function() {
-    // Savoir si l'utilisateur a déjà liké le post ?
-    //TODO
-
-    // Récupérer les 10 derniers commentaires du post
-    var loop  = true;
-    var route = apiRoot() + 'planets/1/posts/' + this.post.id + "/comments?limit=" + this.loadMore;
-    this.$http.get(route, { emulateJSON: true }).then(
+    },
+    getLikes : function() {
+      this.$http.get(apiRoot() + 'posts/' + this.post.id + "/stardust", { emulateJSON: true}).then(
+        (response) => {
+          this.nbLikes = response.data.count;
+        },
+        (response) => {
+          console.log(response)
+        }
+      );
+    },
+    getComments(route) {
+      this.$http.get(route, { emulateJSON: true }).then(
         (response) => {
           this.comments = response.data;
+          var tmp = response.headers.get("Link").split(",")[1].split(";")[0];
+          this.prevComments = apiRoot() + tmp.substring(2, tmp.length-1);
         },
         (response) => {
           console.log(response);
         }
       );
+    }
+  },
+  created : function() {
+    console.log("Created with post id = " + this.post.id);
+    // Savoir si l'utilisateur a déjà liké le post ?
+    //TODO
 
-    // Pour récupèrer les commentaires suivants, une petite idée de ce qu'il va falloir faire
-    //while (loop) {
-      /*this.$http.get(route, { emulateJSON: true }).then(
-        (response) => {
-            this.comments = response.data;
-            console.log("id " + response.data[0].id);
-            var link = response.headers.get("Link").split(",")[1].split(";")[0];
-            route = apiRoot() + link.substring(2, link.length-1);
-            console.log("New route 1 : " + route);
-          }
-        },
-        (response) => {
-          console.log(response);
-        }
-        );*/
+    // Récupérer les 10 derniers commentaires du post
+    this.getComments(apiRoot() + "planets/1/posts/" + this.post.id + "/comments?limit=" + this.loadMore);
 
     // Récupérer le nombre de likes
-    this.$http.get(apiRoot() + 'posts/' + this.post.id + "/stardust", { emulateJSON: true}).then(
-      (response) => {
-        this.nbLikes = response.data.count;
-      },
-      (response) => {
-        console.log(response)
-      });
-    },
+    this.getLikes();
+  },
+  updated: function() {
+    if (this.new) {
+      this.new = false;
+
+      this.getComments(apiRoot() + "planets/1/posts/" + this.post.id + "/comments?limit=" + this.loadMore);
+      this.getLikes();
+    }
+  },
   data () {
     return {
       comments : {},
       nbLikes : 0,
       newComment : '',
+      prevComments: '',
       loadMore : 5,
       showFullPost : false,
       /* admin / earth / parallel / robots / aliens / space-opera*/
       planetData: ["#3e3e3e", "#3eb6df", "#ef4646", "#767fe2", "#72b51a", "#f9a519" ]
+    }
+  },
+  computed: {
+    reversedComments : function() {
+      return this.comments.reverse();
     }
   }
 });
